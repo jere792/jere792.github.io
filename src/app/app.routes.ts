501b6e214b36c4dfd058464871a6d14c06cf3b41
jere792 <138648408+jere792@users.noreply.github.com>
2026-01-12 20:04:22 -0500

    enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    { path: '', redirectTo: 'home', pathMatch: 'full'
   },
   { path: 'about', loadComponent: () => import('./features/about/about').then(m => m.About) },
   { path: 'contact', loadComponent: () => import('./features/contact/contact').then(m => m.Contact) },
   { path: 'home', loadComponent: () => import('./features/home/home').then(m => m.Home) },
   { path: 'projects', loadComponent: () => import('./features/projects/projects').then(m => m.Projects) },
   //{ path: 'experience', loadComponent: () => import('./features/experience/experience').then(m => m.Experience) }
+
+  { path: '**', redirectTo: '/home' }
 ];
